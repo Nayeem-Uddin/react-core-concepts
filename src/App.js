6b95376d64d4c02f557b6ab0c647ff1204457978
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-23 12:17:13 +0600

    similar component

--- src/App.js
+++ src/App.js
@@ -2,42 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const person1 = {
-    name: 'sadia',
-    profession: 'Foodist'
-  }
-  const person2 = {
-    name: 'nayeem',
-    profession: 'IT man'
-  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Exited to learn React
-        </a>
-        <div className="simple-style">
-          <div>
-            <h3>First person name is {person1.name}</h3>
-            <p>She is a {person1.profession}</p>
-          </div>
-          <div>
-            <h3>Second person name is {person2.name}</h3>
-            <p>He is an {person2.profession}</p>
-          </div>
-        </div>
+        <p>Sadia is a reactful person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
+function Person() {
+  return (
+    <div className="sadia-style">
+      <div>
+        <h3>Sadia islam kanchi</h3>
+        <p>She has igo problem</p>
+      </div>
+    </div>
+  );
+}
+
 
 export default App;
